b7f5cae4a0889dbe2d269f5c111551b1daf6158e
melvin~ <melvidfdsa~>
2024-05-17 23:16:44 +0800

    Add project files.

---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SportsStore.Models {
+
+    public static class SeedData {
+
+        public static void EnsurePopulated(IApplicationBuilder app) {
+            StoreDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider
+                .GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any()) {
+                context.Database.Migrate();
+            }
+
+           
+if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description =
+                            "A boat for one person",
+                        Category = "Watersports",
+                        Price = 499.95m
+                    },
+                    new Product
+                    {
+                        Name = "Lifejacket",
+                        Description = "Protective and fashionable",
+                        Category = "Watersports",
+                        Price = 249.95m
+                    },
+                    new Product
+                    {
+                        Name = "Soccer Ball",
+                        Description = "FIFA-approved size and weight",
+                        Category = "Soccer",
+                        Price = 399.95m
+                    },
+                    new Product
+                    {
+                        Name = "Corner Flags",
+                        Description =
+                          "Give your playing field a professional touch",
+                        Category = "Soccer",
+                        Price = 999.95m
+                    },
+                    new Product
+                    {
+                        Name = "Stadium",
+                        Description = "Flat-packed 35,000-seat stadium",
+                        Category = "Soccer",
+                        Price = 99499.95m
+                    },
+                    new Product
+                    {
+                        Name = "Thinking Cap",
+                        Description = "Improve brain efficiency by 75%",
+                        Category = "Chess",
+                        Price = 699.95m
+                    },
+                    new Product
+                    {
+                        Name = "Unsteady Chair",
+                        Description =
+                          "Secretly give your opponent a disadvantage",
+                        Category = "Chess",
+                        Price = 399.95m
+                    },
+                    new Product
+                    {
+                        Name = "Human Chess Board",
+                        Description = "A fun game for the family",
+                        Category = "Chess",
+                        Price = 299.95m
+                    },
+                    new Product
+                    {
+                        Name = "Bling-Bling King",
+                        Description = "Gold-plated, diamond-studded King",
+                        Category = "Chess",
+                        Price = 199.95m
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
